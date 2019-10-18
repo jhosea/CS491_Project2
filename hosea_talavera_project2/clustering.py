65e4f58c150bc16ca950cf756e9b28dd533d0e61
Josh Hosea <jphosea4@gmail.com>
2019-10-18 11:36:47 -0700

    Finished clustering.py

--- hosea_talavera_project2/clustering.py
+++ hosea_talavera_project2/clustering.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import collections
 
 
 #Finds the distance between X and cluster_center, returns an integer
@@ -17,39 +18,47 @@ def find_distance(X, cluster_center):
 '''
 def update_cluster_centers(X, cluster_centers):
 
-	print("Old Cluster Center: ", cluster_centers)
+	#print("Old Cluster Center: ", cluster_centers)
 	closest_center_index = np.array([])
 
 
+	#Iterates through every sample in X
 	for sample in X:
 
 		#Sets the distance of the best distance to the distance between the sample and the first cluster center
 		best_cluster_distance = find_distance(sample,cluster_centers[0])
 		best_cluster_index = 0
 
+
+		#Iterates through each index in cluster centers
 		for index in range(len(cluster_centers)):
 		
+			#Finds the new cluster distance
 			new_cluster_distance = find_distance(sample,cluster_centers[index])
 
 
 			if new_cluster_distance < best_cluster_distance:
 
+				#If the distance from the sample to the cluster center is less than the distance to the previous cluster center 
 				best_cluster_distance = new_cluster_distance
 				best_cluster_index = index
 
-
+		#append the cluster center with  the shortest distance
 		closest_center_index = np.append(closest_center_index,best_cluster_index)
 
 
 
-
+	#Initialize the new cluster centers array
 	new_cluster_centers = np.array([])
 
+	#Iterate through all cluster centers
 	for index in range(len(cluster_centers)):
 
+		#Recalculate the cluster center
 		new_cluster_centers = np.append(new_cluster_centers, np.mean(X[closest_center_index == index], axis = 0))
 
 
+	#returns new cluster centers
 	return new_cluster_centers
 
 
@@ -60,7 +69,7 @@ Parameters:
 	X -> np.array of training data
 	K -> integer number of cluster centers
 Returns:
-
+	Cluster Centers -> sorted np.array
 '''
 def K_Means(X,K):
 
@@ -92,15 +101,47 @@ def K_Means(X,K):
 		old_cluster_centers = new_cluster_centers
 		new_cluster_centers = update_cluster_centers(X, old_cluster_centers)
 
-
+	#returns the new cluster centers, sorted
 	return np.sort(new_cluster_centers)
 
 
+'''
+Finds K cluster centers for X
+Parameters:
+	X -> np.array of training data
+	K -> integer number of cluster centers
+Returns:
+	Cluster Centers -> sorted np.array
+'''
+def K_Means_better(X,K):
+
+	#Creates an np.array of zero's with 1000 rows and K columns
+	all_cluster_centers = np.zeros((1000, K))
+
+	#Creates 1000 cluster centers and puts them in all_cluster_centers
+	for row_index in range(len(all_cluster_centers)):
+
+		all_cluster_centers[row_index,:] = K_Means(X,K)
 
 
-X = np.array([[0], [1], [2], [7], [8], [9], [12], [14], [15]])
+	#Finds all unique values in all_cluster_centers and their counts
+	unique = np.unique(all_cluster_centers, axis = 0, return_counts = True)
 
-print(K_Means(X, 3))
+	#Extracts the counts of the unique values from the tuple unique.
+	unique_counts = unique[1]
+
+	#The 0th elements of unique counts corresponds to the 0th row of unique values
+	unique_values = unique[0]
+
+
+	#Finds the cluster center that was returned most often
+	best_cluster_center = unique_values[np.argmax(unique_counts),:]
+
+
+	#returns the cluster center that occures the most in all_cluster_centers
+	return np.sort(best_cluster_center)
+
+
 
 
 
