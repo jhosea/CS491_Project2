a47787500f5f81e62926fd8bc91f2e5efd0a90c3
Gabrielle Talavera <43487903+gabrielle9talavera@users.noreply.github.com>
2019-10-18 20:56:07 -0700

    clustering tests

--- hosea_talavera_project2/clustering.py
+++ hosea_talavera_project2/clustering.py
@@ -65,7 +65,7 @@ Returns:
 def K_Means(X,K):
 
 	#Initalize cluster_centers array
-	cluster_centers = np.array([])
+	cluster_centers =z
 
 	#Initalizes K cluster centers
 	for count in range(K):
@@ -74,6 +74,7 @@ def K_Means(X,K):
 		new_cluster = random.choice(X)
 
 		#Randomly picks a new point in X to be a cluster center, until it finds a point that has not been used already
+
 		while new_cluster in cluster_centers:
 
 			new_cluster = random.choice(X)
@@ -87,7 +88,7 @@ def K_Means(X,K):
 
 
 	#While new cluster centers are different than the old cluster centers, continue updating the cluster centers
-	while((old_cluster_centers != new_cluster_centers).all()):
+	while(np.array_equal(old_cluster_centers,new_cluster_centers)):
 
 		old_cluster_centers = new_cluster_centers
 		new_cluster_centers = update_cluster_centers(X, old_cluster_centers)
@@ -98,7 +99,41 @@ def K_Means(X,K):
 
 
 
+<<<<<<< Updated upstream
 X = np.array([[0], [1], [2], [7], [8], [9], [12], [14], [15]])
+=======
+def main():
+
+	#test K_Means and K_Means better
+
+
+	test_set_1 = np.array([[0], [1], [2], [7], [8], [9], [12], [14], [15]])
+	test_set_2 = np.array([[1, 0], [7, 4], [9, 6], [2, 1], [4, 8], [0, 3], [13, 5], [6, 8], [7, 3], [3, 6], [2, 1], [8, 3], [10, 2], [3, 5], [5, 1], [1, 9], [10, 3], [4, 1], [6, 6], [2, 2]])
+
+	centers_1_2 = K_Means(test_set_1, 2)
+	centers_1_3 = K_Means(test_set_1, 3)
+
+	centers_2_2 = K_Means(test_set_2, 2)
+	# centers_2_3 = K_Means(test_set_2, 3)
+
+	# better_centers_1_2 = K_Means_better(test_set_1, 2)
+	# better_centers_1_3 = K_Means_better(test_set_1, 3)
+	#
+	# better_centers_2_2 = K_Means_better(test_set_2, 2)
+	# better_centers_2_3 = K_Means_better(test_set_2, 3)
+	#
+	# print("Classification Using K = 1:", centers_1_2)
+	# print("Classification Using K = 2:", centers_1_3)
+	# print("Classification Using K = 5:", centers_2_2)
+	# print("Classification Using K = 1:", centers_2_3)
+	# print("Classification Using K = 2:", better_centers_1_2)
+	# print("Classification Using K = 5:", better_centers_1_3)
+	# print("Classification Using K = 2:", better_centers_2_2)
+	# print("Classification Using K = 5:", better_centers_2_3)
+
+if __name__ == "__main__":
+    main()
+>>>>>>> Stashed changes
 
 print(K_Means(X, 3))
 
